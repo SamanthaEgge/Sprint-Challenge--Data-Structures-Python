c943136ca63896dfdaa3c8587d9ea2476e11b5f8
Samantha Egge <samantha.c.egge@gmail.com>
2020-04-24 13:33:49 -0700

    Completed ring_buffer challenge.

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -6,17 +6,39 @@ class RingBuffer:
     self.capacity = capacity
     self.current = None
     self.storage = DoublyLinkedList()
-    self.size = 0
+    self.index = self.capacity-1
 
-  def __len__(self):
-    return self.size
+  # def __len__(self):
+  #   return self.size
+
+  def getNth(self, tail_node, index): 
+    current = tail_node # Initialise temp 
+    count = self.capacity-1 # Index of current node 
+    print('index in getNth')
+    print(index)
+    print(count)
+    # Loop while end of linked list is not reached 
+    while (current): 
+        if (count == index): 
+            return current 
+        count -= 1
+        current = current.prev
+
+    ## start with e, d, c, b, a
+    ## index [last] is capacity - 1 (a) (tail node)
+    ## e, d, c, b, f
+    ## capacity [-2] is second oldest element, b
+
+    # if we get to this line, the caller was asking 
+    # for a non-existent element so we assert fail 
+
 
   def append(self, item):
     # Need to maintain a DLL of the length of the capacity
     # Continue adding to DLL until it matches capacity.
-    print('in append')
-    print('item')
-    print(item)
+    # print('in append')
+    # print('item')
+    # print(item)
     # print(self.capacity)
     if self.storage.length < self.capacity:
       self.storage.add_to_head(item)
@@ -26,10 +48,24 @@ class RingBuffer:
       # 3rd a, b, c
       # etc, etc. Adding nodes to end end of list ensures order is kept.
     # Once limit is reached. the oldest node (the head), must be removed, and another added to the tail
+      # self.storage.remove_from_tail()
+      # self.storage.add_to_head(item)
     else:
-      print('???')
-      self.storage.remove_from_tail()
-      self.storage.add_to_head(item)
+      if self.index > 0:
+        node_placement = self.getNth(self.storage.tail, self.index)
+        print('node value')
+        print(node_placement.value)
+        node_placement.value = item
+        # node_placement.insert_before(item)
+        self.index = self.index-1
+        print('else')
+        print(self.index)
+      else: 
+        print('resetting index')
+        self.index = self.capacity-1
+        self.storage.remove_from_head()
+        self.storage.add_to_head(item)
+
 
     
 
@@ -45,6 +81,11 @@ class RingBuffer:
       current = current.prev
       count +=1
     return list_buffer_contents
+
+### Next steps. Have an index that compares max capacity vs, which index the oldest or newest item is on. Use that to determine where to delete items within the dll
+### then, this should work?
+
+
 
 # ----------------Stretch Goal-------------------
 
